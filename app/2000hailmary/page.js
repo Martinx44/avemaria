d2bd96db28280353b735de04c422494b04b74a54
martin agu <agu.chukwuagozie01@gmail.com>
2024-09-29 05:19:05 +0100

    Add react-icons package

--- app/2000hailmary/page.js
+++ app/2000hailmary/page.js
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import Image from "next/image";
+
 import NextButton from '@/components/NextButton';
 import PrevButton from '@/components/PrevButton';
 import backgroundImage from "@/assets/maria.png";
